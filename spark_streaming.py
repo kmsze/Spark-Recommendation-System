2e251e4b01b9373ac48aebbf933e3cc1b9455098
Ubuntu <tom@vm1.mcsqxdb5afquphyseo1i2dx1uf.ix.internal.cloudapp.net>
2018-12-20 08:03:35 +0000

    cleaned up

--- spark_streaming.py
+++ spark_streaming.py
@@ -6,15 +6,15 @@ from pyspark.ml.recommendation import ALS
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
 
-sc = SparkContext("spark://ks1:7077", "StreamProcessing3")
-sc.addPyFile('/home/kmsze/Spark-Recommendation-System/db_connector.py')
+sc = SparkContext("spark://vm1:7077", "StreamProcessing")
+sc.addPyFile('/home/tom/Spark-Recommendation-System/db_connector.py')
 sc.setLogLevel("ERROR")
 
 from db_connector import DBConnector
 
-TRIGGER_INTERVAL = 1  # in seconds
+TRIGGER_INTERVAL = 30  # in seconds
 TOPIC_NAME = 'spark_streaming'
-KAFKA_PORT = 'ks1:2181'
+KAFKA_PORT = 'vm1:2181'
 db = DBConnector('streaming_db')
 
 def fit_model(df):
@@ -47,30 +47,16 @@ def process(rdd):
     else:
         print("started processing")
         incoming_data = rdd.toDF(["userId","movieId","rating"])
-        # print('*************1')
         existing_data = db.fetch_raw_data_from_user_ratings_table()
-        # print('*************2')
         existing_data = existing_data.drop('_id')
-        # print('*************3')
         # use select to make sure the columns are in the same order before union
         incoming_data = incoming_data.select("userId","movieId","rating")
-        # print('*************4')
         existing_data = existing_data.select("userId","movieId","rating")
         # merge the two dataframes
-        # print('*************5')
         combined_data = existing_data.union(incoming_data)
-        # combined_data.rdd.repartition(8)
-        # print("number of partitions: %s" % combined_data.rdd.getNumPartitions())
-        # print("incoming data:")
-        # incoming_data.show(100, truncate=False)
-        # print("existing_data count: %s" % existing_data.count())
-        # existing_data.show(200, truncate=False)
-        # print("combined_data count: %s" % combined_data.count())
-        # combined_data.show(200, truncate=False)
         print("training model on combined data...")
         model = fit_model(combined_data)
         predictions = model.recommendForAllUsers(5)
-        # might need further processing of userRecs
         db.add_new_user_rating(incoming_data)
         print("stored incoming data, storing results...")
         db.store_prediction_results(predictions)
