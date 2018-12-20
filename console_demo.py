7b384996c1ab3201b6bc717455d53f5a592dcdbd
Kenneth Sze <kmsze@github.com>
2018-12-20 16:01:51 +0800

    added comments and clean up unused code

--- console_demo.py
+++ console_demo.py
@@ -16,30 +16,27 @@ if __name__ == "__main__":
         if user_prediction is None:
             return ["Dummy 1", "Dummy 2", "Dummy 3", "Dummy 4", "Dummy 5"]
         movie_id_list = user_prediction["movies_id_list"]
-        #print(movie_id_list)
         movies_list = []
         for id in movie_id_list:
-            #print(movies_info.find_one({'movieId': id}))
             movies_list.append(str(movies_info.find_one({'movieId': id})['title']))
         return movies_list
     
-    fun = ""
-    while (fun != "exit"):
-        fun = input("Get movies recommendation for which user (user ID)?\n")
+    user_input = ""
+    while (user_input != "exit"):
+        user_input = input("Get movies recommendation for which user (user ID)?\n")
 
         user_id = ""
         try:
-            user_id = int(fun)
+            user_id = int(user_input)
         except ValueError:
             pass
 
         if type(user_id) == int:
             movies_list = get_recommendations(user_id)
             for index, movie_title in enumerate(movies_list):
-                print "{0}. {1}".format(index + 1, movie_title)
-                #print("%d. %s" % (index + 1, movie_title))           
+                print "{0}. {1}".format(index + 1, movie_title)          
             print("")
-        elif fun == "exit":
+        elif user_input == "exit":
             pass
         else:
             #pass
--- db_connector.py
+++ db_connector.py
@@ -15,10 +15,12 @@ class DBConnector:
                     .config("spark.mongodb.input.uri", DBConnector.MONGO_DB_URI)\
                     .config("spark.mongodb.output.uri", DBConnector.MONGO_DB_URI)\
                     .getOrCreate()
+        # Get all the movie information from the database collection to a dataframe and cache it for later usage
         self.df_movies_info = self.spark.read.format("com.mongodb.spark.sql.DefaultSource").option("collection", "MoviesInfoDemo").load()
         self.df_movies_info.cache()
     
     def fetch_raw_data_from_user_ratings_table(self, get_small_dataset=True):
+        # Load existing and new user ratings from the MongoDB database to dataframes
         if get_small_dataset:
             df_user_ratings = self.spark.read.format("com.mongodb.spark.sql.DefaultSource").option("collection", "UserRatingsDemo").load()
         else:
@@ -29,11 +31,10 @@ class DBConnector:
         df_user_ratings = df_user_ratings.select("userId", "movieId", "rating")
         df_new_user_ratings = df_new_user_ratings.select("userId", "movieId", "rating")
         
-        #df_user_ratings.show()
-        #df_new_user_ratings.show()
-        
+        # Combine existing user ratings with new user ratings
         df_combined_user_ratings = df_new_user_ratings.union(df_user_ratings)
-        #df_combined_user_ratings.show()
+        df_combined_user_ratings.cache()
+
         return df_combined_user_ratings
 
     def _add_new_user_rating(self, user_id, movie_id, ratings):
@@ -41,13 +42,19 @@ class DBConnector:
         self.add_new_user_rating(df_new_rating)
 
     def add_new_user_rating(self, df_new_rating):
+        # Append new user rating data to the user ratings collection
         df_new_rating.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").option("collection", "UserRatingsNew").save()
     
     def get_recommendations(self, user_id):
+        # Read the prediction result from the PredictionResults collection to a dataframe and cache it
         self.df_result = self.spark.read.format("com.mongodb.spark.sql.DefaultSource").option("collection","PredictionResults").load()
         self.df_result.cache()
+
+        # Get the recommended movie id list for a specific user ID
         movie_id_list = self.df_result.select('movies_id_list').where("userId = " + str(user_id)).collect()
         movies_list = movie_id_list[0][0]
+
+        # Get the corresponding movie title for each movie ID
         udf_filter = udf(lambda x: int(x) in movies_list, BooleanType())
         movies_title = self.df_movies_info.select("title").filter(udf_filter("movieId")).collect()
         return [x.title for x in movies_title]
@@ -57,7 +64,6 @@ class DBConnector:
         train, validation, test = ratings.randomSplit([0.7, 0.2, 0.1])
         als = ALS(maxIter=20, regParam=0.01, rank=8, userCol="userId", itemCol="movieId", ratingCol="rating", coldStartStrategy="drop")
         model = als.fit(train)
-        #predictions = model.transform(validation)
         df_prediction = model.recommendForAllUsers(5)
         self.store_prediction_results(df_prediction)
 
@@ -65,6 +71,7 @@ class DBConnector:
         def movie_id_list(x):
             return [int(val.movieId) for val in x]
         
+        # Overwrite existing prediction result in the database with new prediction result from the model
         movie_id_list_udf = udf(lambda y: movie_id_list(y), ArrayType(IntegerType()))
         df_prediction = df_prediction.select('*', movie_id_list_udf('recommendations').alias("movies_id_list"))
         df_prediction = df_prediction.drop("recommendations")
